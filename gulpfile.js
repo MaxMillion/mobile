270caf3e51f5bc7d5ecdb9f63757c9d05e3b61f0
Shahen Ohanjanyan <shabunc@gmail.com>
2014-12-01 02:57:10 +0300

    introducing gulp

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+
+
+var jade = require('gulp-jade');
+
+gulp.task('html', function() {
+
+  gulp.src('./src/html/*.jade')
+    .pipe(jade({
+    }))
+    .pipe(gulp.dest('./build/'))
+});
+
+
+gulp.task('templates', function() {
+  gulp.src('./src/js/*.jade')
+    .pipe(jade({
+      client: true
+    }))
+    .pipe(gulp.dest('./build/tmp/templates'))
+});
+
+var compass = require('gulp-compass');
+var path = require('path');
+
+gulp.task('compass', function() {
+  gulp.src('./src/sass/*.scss')
+    .pipe(compass({
+      project: path.join(__dirname, 'src'),
+      sass: 'sass',
+      css: 'build/css'
+    }))
+    .pipe(gulp.dest('./build/css'));
+});
+
+var amdOptimize = require("amd-optimize");
+
+gulp.task("js", function () {
+
+  return gulp.src("src/js/modules/**/*.js")
+    // Traces all modules and outputs them in the correct order.
+    .pipe(amdOptimize("app"))
+    .pipe(gulp.dest("./build/js"));
+
+});
+
+
+gulp.task('default', ['compass', 'html', 'templates', 'js'], function() {
+  // place code for your default task here
+});
---
+++ src/js/modules/app.js
@@ -0,0 +1,5 @@
+
+
+define(['dep'], function() {
+    alert('HEY!');   
+})
